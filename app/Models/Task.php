133d57f98e4c7f9db2c439078931f33036edecd9
Bahit Hamid <bahit.hamid@pb.edu.bn>
2025-08-16 10:23:30 +0800

    Fix error where $fillable was needed

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+    ];
 }
