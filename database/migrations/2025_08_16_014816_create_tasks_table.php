0490004ae886639313057cc73d97e174a488cd63
Bahit Hamid <bahit.hamid@pb.edu.bn>
2025-08-16 09:55:19 +0800

    Add columns for title, description, and status to tasks table migration

--- database/migrations/2025_08_16_014816_create_tasks_table.php
+++ database/migrations/2025_08_16_014816_create_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['Pending', 'Completed'])->default('Pending');
             $table->timestamps();
         });
     }
